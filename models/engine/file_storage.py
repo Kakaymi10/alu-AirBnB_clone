f81fef4343a1f093758bd3994c1a397cdc4cf39f
Kakaymi10 <m.moussa@alustudent.com>
2023-05-20 06:52:20 -0700

    Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,8 @@ import json
 
 class FileStorage:
     def __init__(self):
-    __file_path = "file.json"
-    __objects = {}
+        sel.__file_path = "file.json"
+        self. __objects = {}
 
     def all(self):
         return self.__objects
