0bf32c6137d3371a410ec3a8b380507311fb9564
Kakaymi10 <m.moussa@alustudent.com>
2023-05-25 23:19:16 -0700

    Fixed Json

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
@@ -34,7 +39,12 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     __all_classes = {
             "BaseModel",
-            "User"
+            "User",
+            "State",
+            "City",
+            "Place",
+            "Amenity",
+            "Review"
     }
 
     def do_quit(self, arg):
